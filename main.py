fc5ffcc5ca193d0cc4fff4bddfd05042071bcdad
Antonio Reyes <antrey717@gmail.com>
2023-04-30 16:47:25 -0500

    Added functions: Deposit, Withdraw

--- main.py
+++ main.py
@@ -4,7 +4,61 @@ connection = mysql.connector.connect(user='root', database='bank', password='ant
 
 cursor = connection.cursor()
 
-testQuery = ("SELECT * FROM info")
+import random
+
+print("--Welcome to my bank!--")
+
+repeat = True
+
+testQuery = ('SELECT Balance FROM info')
+balance = cursor.execute(testQuery)
+balance = int(balance)
+acc_balance = [balance]
+acc_balance = sum(acc_balance)
+  
+def deposit():
+    print("**Depositing**")
+    print("------------------")
+    dep_amt = int(input("How much would you like to deposit? "))
+
+    acc_balance.append(dep_amt)
+    new_bal = sum(acc_balance)
+  
+    print(f'Your balance is now {new_bal}!')
+    return
+  
+def withdraw():
+    wit_amt = int(input("How much would you like to withdraw? "))
+  
+    balance = sum(acc_balance)
+    if balance < wit_amt:
+      print("Error... \nYour account balance is low.")
+    elif balance >= wit_amt:
+      new_bal = balance - wit_amt
+      acc_balance.clear()
+      acc_balance.append(new_bal)
+      print(f'Your balance is now {new_bal}.')
+    return
+    
+    
+while repeat:
+    
+    prompt = input("What can I help you with today? ")
+  
+    if 'balance' in prompt:
+      print (acc_balance)
+  
+    elif 'deposit' in prompt:
+      deposit()
+      
+    elif 'withdraw' in prompt:
+      withdraw()
+  
+    elif 'nothing' in prompt:
+      print("Goodbye!")
+    else:
+      print("Choose 'balance', 'deposit', 'withdraw', or 'nothing'")
+    
 
 cursor.execute(testQuery)
 
--- testing.py
+++ testing.py
@@ -1,51 +1,20 @@
-from tkinter import *
+import mysql.connector
 
-root = Tk()
-root.title("Simple Calculator")
+connection = mysql.connector.connect(user='root', database='bank', password='antonio2008.')
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+cursor = connection.cursor()
 
-#e.insert(0, "Enter your name: ")
+testQuery = ('SELECT Balance FROM info')
 
-def button_add():
-    return
-def button_equal():
-    return
-def button_clear():
-    return
+cursor.execute(testQuery)
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+for item in cursor:
+    for x in item:
+        if x.isdigit:
+            balance = x
 
-button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_add)
-button_clear = Button(root, text="clear", padx=79, pady=20, command=button_add)
+print(balance)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+cursor.close()
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-
-button_0.grid(row=4, column=0)
-
-button_add.grid(row=5, column=0)
-button_equal.grid()
-button_clear()
-
-root.mainloop()
+connection.close()
